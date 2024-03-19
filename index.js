0884e1c04ff148582513ce4b9a5a5d8e0fb9a551
rayhan gustiar <rayhangustiar@gmail.com>
2024-03-19 11:06:55 +0700

    test deploy

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Test commit --> auto deploy')
 })
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3500)
